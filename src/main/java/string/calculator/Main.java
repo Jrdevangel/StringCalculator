ece2f94398ae87271a75f3525af7f3265549967a
IsamarRB <romerobellorini@gmail.com>
2024-08-27 19:12:52 +0200

    Multiply-divide code

---
+++ src/main/java/string/calculator/Main.java
@@ -0,0 +1,34 @@
+package string.calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to StringCalculator Kata!");
+
+        System.out.println("We need to test the methods of the calculator\n" +
+
+                "Add: think of two numbers less than 1000");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the first number: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Please enter the second number: ");
+        int number2 = scanner.nextInt();
+
+
+        if (number1 < 1000 && number2 < 1000) {
+            StringCalculator calculator = new StringCalculator();
+            int result = calculator.add(number1 + "," + number2);
+            System.out.println("Result: " + result);
+        } else {
+            System.out.println("Both numbers must be less than 1000.");
+        }
+
+        System.out.println("Thank you!");
+
+        scanner.close();
+    }
+}
